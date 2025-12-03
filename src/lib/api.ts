ad88b221af32945e19263cda26de77307420413f
Realm Bots <nomanabdullah01010@gmail.com>
2025-12-03 16:56:54 +0600

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -14,6 +14,18 @@ export const getDirectory = async (path: string, password: string) => {
   return api.post("/api/getDirectory", { path, password });
 };
 
+export const createNewFolder = async (path: string, name: string, password: string) => {
+  return api.post("/api/createNewFolder", { path, name, password });
+};
+
+export const renameFileFolder = async (path: string, name: string, password: string) => {
+  return api.post("/api/renameFileFolder", { path, name, password });
+};
+
+export const deleteFileFolder = async (path: string, password: string) => {
+  return api.post("/api/deleteFileFolder", { path, password });
+};
+
 export const getFileDownloadUrl = (path: string, id: string) => {
   return `${API_URL}/file?path=${path}${id}`;
 };
